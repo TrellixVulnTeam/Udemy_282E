ee98ba22ff77d13ca9c25ba80d2d879de07804c2
jbarcaza <jbarcaza@gmail.com>
2021-08-17 21:36:54 -0400

    Class 94

--- spring5/angular/clientes-app/src/app/app.module.ts
+++ spring5/angular/clientes-app/src/app/app.module.ts
@@ -14,6 +14,10 @@ import localeES from '@angular/common/locales/es';
 import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+//import {MatNativeDateModule} from '@angular/material/core';
+import {MatMomentDateModule } from '@angular/material-moment-adapter';
 
 registerLocaleData(localeES,'es');
 
@@ -40,7 +44,10 @@ const routes : Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
   providers: [ClienteService,{provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
--- spring5/spring/spring-boot-backend-apirest/src/main/java/com/telimay/spring/boot/backend/apirest/models/entity/Cliente.java
+++ spring5/spring/spring-boot-backend-apirest/src/main/java/com/telimay/spring/boot/backend/apirest/models/entity/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
@@ -41,16 +42,17 @@ public class Cliente implements Serializable {
 	@Email(message="debe tener formato de email")
 	private String email;
 	
+	@NotNull(message="no puede ser vacío")
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	@PrePersist
-	public void prePersist() {
-		
-		createAt = new Date();
-		
-	}
+//	@PrePersist
+//	public void prePersist() {
+//		
+//		createAt = new Date();
+//		
+//	}
 
 	public Long getId() {
 		return id;
